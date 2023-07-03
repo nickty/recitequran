0f8a71de8357d706d75768fff0aa674a61d107df
Mizanur Rahman <mizan@arogga.net>
2023-07-03 15:07:02 +0600

    Just before repeat mode

--- screens/QuranPlayer.js
+++ screens/QuranPlayer.js
@@ -3,6 +3,9 @@ import TrackPlayer, {
   State,
   usePlaybackState,
   Capability,
+  useProgress,
+  useTrackPlayerEvents,
+  Event,
 } from 'react-native-track-player';
 import {
   StyleSheet,
@@ -40,15 +43,21 @@ const QuranPlayer = () => {
   const [suraInext, setSuraIndex] = useState(0);
   const scrollx = useRef(new Animated.Value(0)).current;
 
-  const setUpPlayer = async () => {
-    try {
-      await TrackPlayer.setupPlayer();
-      await TrackPlayer.add(songs);
-      console.log('truck added');
-    } catch (error) {
-      console.log(error);
+  const progress = useProgress();
+
+  // changing the track
+  const [title, setTitle] = useState();
+  const [artist, setArtist] = useState();
+  const [artwork, setArtwork] = useState();
+  useTrackPlayerEvents([Event.PlaybackTrackChanged], async event => {
+    if (event.type === Event.PlaybackTrackChanged && event.nextTrack !== null) {
+      const track = await TrackPlayer.getTrack(event.nextTrack);
+      const {title, artist, artwork} = track;
+      setTitle(title);
+      setArtist(artist);
+      setArtwork(artwork);
     }
-  };
+  });
 
   useEffect(() => {
     // TrackPlayer.updateOptions({
@@ -64,17 +73,45 @@ const QuranPlayer = () => {
       // console.log(value);
       const index = Math.round(value / width);
       console.log(index);
+      skipTo(index);
       setSuraIndex(index);
     });
 
     // return () => TrackPlayer.destroy();
   }, []);
 
+  const skipTo = async trackId => {
+    await TrackPlayer.skip(trackId);
+  };
+
+  const setUpPlayer = async () => {
+    try {
+      await TrackPlayer.setupPlayer();
+      await TrackPlayer.add(songs);
+      console.log('truck added');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const suraSlider = useRef(null);
+
+  const skipToNext = () => {
+    suraSlider.current.scrollToOffset({
+      offset: (suraInext + 1) * width,
+    });
+  };
+  const skipToPrev = () => {
+    suraSlider.current.scrollToOffset({
+      offset: (suraInext - 1) * width,
+    });
+  };
+
   const renderSuras = ({item, index}) => {
     return (
       <Animated.View style={styles.mainImageWrapper}>
         <View style={styles.imageWrapper}>
-          <Image source={item.artwork} style={styles.musicImage} />
+          <Image source={artwork} style={styles.musicImage} />
         </View>
       </Animated.View>
     );
@@ -83,6 +120,7 @@ const QuranPlayer = () => {
     <SafeAreaView style={styles.container}>
       <View style={styles.mainContainer}>
         <Animated.FlatList
+          ref={suraSlider}
           renderItem={renderSuras}
           data={songs}
           keyExtractor={item => item.id}
@@ -105,29 +143,39 @@ const QuranPlayer = () => {
         />
 
         <View style={styles.suraContent}>
-          <Text style={styles.suraTitle}>{songs[suraInext].title}</Text>
-          <Text style={styles.suraArtist}>{songs[suraInext].artist}</Text>
+          <Text style={styles.suraTitle}>{title}</Text>
+          <Text style={styles.suraArtist}>{artist}</Text>
         </View>
         <View>
           <Slider
             style={styles.progressBar}
-            value={10}
+            value={progress.position}
             minimumValue={0}
-            maximumValue={100}
+            maximumValue={progress.duration}
             minimumTrackTintColor="#FFFFFF"
             thumbTintColor="#ffd369"
             maximumTrackTintColor="#fff"
-            onSlidingComplete={() => {}}
+            onSlidingComplete={async value => {
+              await TrackPlayer.seekTo(value);
+            }}
           />
           {/* progress duration */}
           <View style={styles.progressDuration}>
-            <Text style={styles.progressLavelText}>0.00</Text>
-            <Text style={styles.progressLavelText}>0.00</Text>
+            <Text style={styles.progressLavelText}>
+              {new Date(progress.position * 1000)
+                .toLocaleTimeString()
+                .substring(3)}
+            </Text>
+            <Text style={styles.progressLavelText}>
+              {new Date(progress.duration - progress.position * 1000)
+                .toLocaleTimeString()
+                .substring(3)}
+            </Text>
           </View>
         </View>
 
         <View style={styles.musicControl}>
-          <TouchableOpacity onPress={() => {}}>
+          <TouchableOpacity onPress={skipToPrev}>
             <Ionicons name="play-skip-back-outline" size={35} color="#ffd369" />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => togglePlayBack(playBackState)}>
@@ -141,7 +189,7 @@ const QuranPlayer = () => {
               color="#888888"
             />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => {}}>
+          <TouchableOpacity onPress={skipToNext}>
             <Ionicons
               name="play-skip-forward-outline"
               size={35}
